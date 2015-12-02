a81e10da3fb9018a762ba7d912157402f7ae9a80
Miki Rezentes <miki@spreedly.com>
2015-12-02 16:30:38 -0500

    Clean up

--- vendor/bundle/ruby/2.1.0/gems/openssl-pkey-ec-ies-0.0.1/ext/ies/siteconf20151201-47025-ny35k7.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20151201-47025-1dhpibq"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
