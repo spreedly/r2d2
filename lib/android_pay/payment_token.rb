00b7fda1174ee8ca3d9adeb1a5783963ff90bb1b
Miki Rezentes <miki@spreedly.com>
2015-12-02 19:03:27 -0500

    Complete tests

--- lib/android_pay/payment_token.rb
+++ lib/android_pay/payment_token.rb
@@ -7,42 +7,39 @@ module Android_Pay
 
     attr_accessor :data, :ephemeral_public_key, :tag
 
-
     def initialize(token_attrs)
-      self.ephemeral_public_key = token_attrs["ephemeralPublicKey"]
-      self.tag = token_attrs["tag"]
-      self.data = token_attrs["data"]
+      self.ephemeral_public_key = Base64.decode64(token_attrs["ephemeralPublicKey"])
+      self.tag = Base64.decode64(token_attrs["tag"])
+      self.data = Base64.decode64(token_attrs["data"])
     end
 
-    def decrypt(ephemeral_public_key, private_key_pem)
+    def decrypt(private_key_pem)
       digest = OpenSSL::Digest.new('sha256')
       private_key = OpenSSL::PKey::EC.new(private_key_pem)
 
       shared_secret = self.class.generate_shared_secret(private_key, ephemeral_public_key)
 
-      info = "Android".encode 'ASCII'
-
-      key_material = Base64.decode64(ephemeral_public_key) + shared_secret;
-      hkdf = HKDF.new(key_material, :algorithm => 'SHA256', :info => info)
+      key_material = ephemeral_public_key + shared_secret;
+      hkdf = HKDF.new(key_material, :algorithm => 'SHA256', :info => 'Android')
 
       symmetricEncryptionKey = hkdf.next_bytes(16)
       mac_key = hkdf.next_bytes(16)
 
-      verify_mac = OpenSSL::HMAC.digest(digest, mac_key, Base64.decode64(data))
+      verify_mac = OpenSSL::HMAC.digest(digest, mac_key, data)
+      unless verify_mac == tag
+        puts "Invalid TAG, need to handle error"
+      end
       #TODO: handle the error if the tag is wrong
 
       # Return JSON string, up to caller to parse
-      self.class.decrypt(Base64.decode64(data), symmetricEncryptionKey)
+      self.class.decrypt(data, symmetricEncryptionKey)
     end
 
     class << self
 
       def generate_shared_secret(private_key, ephemeral_public_key)
-        epk = ephemeral_public_key;
-        puts epk.encoding
-        decoded_epk = Base64.decode64(epk)
         ec = OpenSSL::PKey::EC.new('prime256v1')
-        bn = OpenSSL::BN.new(decoded_epk, 2) # bn = a Big Number
+        bn = OpenSSL::BN.new(ephemeral_public_key, 2) # bn = a Big Number
         point = OpenSSL::PKey::EC::Point.new(ec.group, bn) # the point on the elliptic curve
         private_key.dh_compute_key(point)
       end
@@ -52,8 +49,8 @@ module Android_Pay
         decipher.decrypt
         decipher.key = symmetric_key
         decipher.auth_data = ""
-        plain = decipher.update(encrypted_data) + decipher.final
-        plain.unpack('U*').collect { |el| el.chr }.join
+        payload = decipher.update(encrypted_data) + decipher.final
+        payload.unpack('U*').collect { |el| el.chr }.join
       end
     end
   end
--- test/payment_token_test.rb
+++ test/payment_token_test.rb
@@ -10,44 +10,29 @@ class Android_Pay::PaymentTokenTest < Test::Unit::TestCase
     @token_attrs = JSON.parse(File.read(fixtures + "token.json"))
     @private_key = File.read(fixtures + "private_key.pem")
     @payment_token = Android_Pay::PaymentToken.new(@token_attrs)
+    @shared_secret = ['44a9715c18ebcb255af705f7332657420aca40604334a7d48a89baba18280a97']
   end
 
   def test_initialize
-    assert_equal @token_attrs["ephemeralPublicKey"], @payment_token.ephemeral_public_key
-    assert_equal @token_attrs["tag"], @payment_token.tag
-    assert_equal @token_attrs["data"], @payment_token.data
-    test_decrypt
+    assert_equal Base64.decode64(@token_attrs["ephemeralPublicKey"]), @payment_token.ephemeral_public_key
+    assert_equal Base64.decode64(@token_attrs["tag"]), @payment_token.tag
+    assert_equal Base64.decode64(@token_attrs["data"]), @payment_token.data
   end
 
 
   def test_decrypt
-    # payment_data = @payment_token.decrypt(@token_attrs["ephemeralPublicKey"], @private_key)
-    # assert_equal "plaintext", payment_data
-    payment_data = JSON.parse(@payment_token.decrypt(@token_attrs["ephemeralPublicKey"], @private_key))
-    puts payment_data
-    # assert_equal "4109370251004320", payment_data["applicationPrimaryAccountNumber"]
-    # assert_equal "200731", payment_data["applicationExpirationDate"]
-    # assert_equal "840", payment_data["currencyCode"]
-    # assert_equal 100, payment_data["transactionAmount"]
-    # assert_equal nil, payment_data["cardholderName"]
-    # assert_equal "040010030273", payment_data["deviceManufacturerIdentifier"]
-    # assert_equal "3DSecure", payment_data["paymentDataType"]
-    # assert_equal "Af9x/QwAA/DjmU65oyc1MAABAAA=", payment_data["paymentData"]["onlinePaymentCryptogram"]
-    # assert_equal "5", payment_data["paymentData"]["eciIndicator"]
+    payment_data = JSON.parse(@payment_token.decrypt( @private_key))
+    assert_equal "4895370012003478", payment_data["dpan"]
+    assert_equal 12, payment_data["expirationMonth"]
+    assert_equal 2020, payment_data["expirationYear"]
+    assert_equal "3DS", payment_data["authMethod"]
+    assert_equal "AgAAAAAABk4DWZ4C28yUQAAAAAA=", payment_data["3dsCryptogram"]
+    assert_equal "07", payment_data["3dsEciIndicator"]
   end
 
+  def test_shared_secret
+    priv_key = OpenSSL::PKey::EC.new(@private_key)
+    assert_equal @shared_secret, Android_Pay::PaymentToken.generate_shared_secret(priv_key, Base64.decode64(@token_attrs["ephemeralPublicKey"])).unpack('H*')
+  end
 
-  # def test_merchant_id
-  #   cert = OpenSSL::X509::Certificate.new(@certificate)
-  #   assert_equal @merchant_id, Android_Pay::PaymentToken.extract_merchant_id(cert)
-  # end
-
-  # def test_shared_secret
-  #   priv_key = OpenSSL::PKey::EC.new(@private_key)
-  #   assert_equal @shared_secret, Android_Pay::PaymentToken.generate_shared_secret(priv_key, @token_attrs["header"]["ephemeralPublicKey"])
-  # end
-
-  # def test_symmetric_key
-  #   assert_equal @symmetric_key, Android_Pay::PaymentToken.generate_symmetric_key(@merchant_id, @shared_secret)
-  # end
 end
