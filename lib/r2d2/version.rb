53c55e9b33bf0a217b8a8665e2d36b52ea8ddbea
Bart de Water <bartdewater@gmail.com>
2017-11-07 10:50:13 -0500

    Add frozen string literal magic comment

--- lib/r2d2/version.rb
+++ lib/r2d2/version.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module R2D2
   VERSION = "0.1.0" unless defined? R2D2::VERSION
 end
--- test/payment_token_test.rb
+++ test/payment_token_test.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require "test_helper"
 
 class R2D2::PaymentTokenTest < Minitest::Test
